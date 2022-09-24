7613fbaf290701c677ab1a5fa92900cf0e6f503c
Mi1ax <yuriymilax@icloud.com>
2022-09-24 19:42:15 +0300

    Added sandbox

---
+++ Sanbox/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Sandbox;
+
+class Program
+{
+    private class Sandbox : Application
+    {
+        
+    }
+    
+    private static void Main()
+    {
+        var sandbox = new Sandbox();
+        sandbox.Run();
+    } 
+}
---
+++ Sandbox/Program.cs
@@ -0,0 +1,35 @@
+﻿using CouscousEngine.CCGui;
+using CouscousEngine.Core;
+using CouscousEngine.Utils;
+using ImGuiNET;
+
+namespace Sandbox;
+
+internal static class Program
+{
+    private class Sandbox : Application
+    {
+        public Sandbox() 
+            : base("Sandbox")
+        {
+            
+        }
+
+        protected override void Update()
+        {
+            Renderer.BeginDrawing();
+            Renderer.ClearBackground(Color.BLACK);
+            
+            rlImGui.Begin();
+            ImGui.ShowDemoWindow();
+            rlImGui.End();
+            Renderer.EndDrawing();
+        }
+    }
+    
+    private static void Main()
+    {
+        var sandbox = new Sandbox();
+        sandbox.Run();
+    }
+}
